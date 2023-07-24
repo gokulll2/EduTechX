d4ee2f808897b627c330c518202d2cc1b92e73bd
Gokul <gokulsethi69@gmail.com>
2023-07-24 20:05:56 +0530

    main

--- BackEnd/models/CourseProgress.js
+++ BackEnd/models/CourseProgress.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const courseProgress = mongoose.Schema({
     courseID:{
-
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Course",
     }
 })
 module.exports = mongoose.model("courseProgress",courseProgress);
