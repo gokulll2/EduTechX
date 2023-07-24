8402abc43d19d0fbdff6e0a8b81d281216d2467d
Gokul <gokulsethi69@gmail.com>
2023-07-25 01:08:17 +0530

    main

---
+++ BackEnd/models/Course.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const courseSchema = mongoose.Schema({
+    courseName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    courseDescription:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    whatYouWillLearn:{
+        type:String,
+    },
+    courseContent:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+            required:true,
+        }
+    ],
+    ratingAndReviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"RatingAndReview",
+            required:true,
+        }
+    ],
+    price:{
+        type:Number,
+        required:true,
+    },
+    thumbnail:{
+        type:String,
+    },
+    tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+    },
+    studentsEnrolled:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"User",
+        }
+    ],
+})
+module.exports = mongoose.model("Course",courseSchema);
