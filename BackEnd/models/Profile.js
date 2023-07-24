1b937ce34dbccc206f9337a3e8b7e8b256a3096a
Gokul <gokulsethi69@gmail.com>
2023-07-24 20:01:10 +0530

    main

--- BackEnd/models/Profile.js
+++ BackEnd/models/Profile.js
@@ -1,1 +1,22 @@
 const mongoose = require("mongoose");
+
+const profileSchema = mongoose.Schema({
+    gender:{
+        type:String,
+        required:true,
+    },
+    dateOfBirth:{
+        type:String,
+        required:true,
+    },
+    about:{
+        type:String,
+        trim:true,
+    },
+    contactNumbers:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+})
+module.exports = mongoose.model("Profile",profileSchema);
