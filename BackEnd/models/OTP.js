7dea39de6710c1c09a46d5c8d5704777c4af624b
Gokul <gokulsethi69@gmail.com>
2023-07-25 16:10:02 +0530

    main

--- BackEnd/models/OTP.js
+++ BackEnd/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 
 const OTPschema = new mongoose.Schema({
@@ -15,5 +16,20 @@ const OTPschema = new mongoose.Schema({
         default:Date.now(),
         expiresIn:5*60,
     }
+})
+
+// a function -> to send emails
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(email , "Verification Email from EduTechX" , otp);
+        console.log("Email Sent Successfully: ",mailResponse)
+    } catch(error){
+        console.log("Error Occurred while sending mails:",error);
+        throw error;
+    }
+}
+OTPschema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
 })
 module.exports = mongoose.model("OTP",OTPschema);
