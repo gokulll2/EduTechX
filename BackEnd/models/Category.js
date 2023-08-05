b7e3d4bd27f6c7f7d863ece4fa1ec8ce3a0fca88
Gokul <gokulsethi69@gmail.com>
2023-08-06 04:18:21 +0530

    main

--- BackEnd/models/Category.js
+++ BackEnd/models/Category.js
@@ -11,4 +11,4 @@ const categorySchema = new mongoose.Schema({
             ref:"Course",
         }],
 })
-model.exports = mongoose.model("Category",categorySchema);
+module.exports = mongoose.model("Category",categorySchema);
