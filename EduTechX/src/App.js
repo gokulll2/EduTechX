20b10248d4ff6862c611218bc447c1768e58d3ed
Gokul <gokulsethi69@gmail.com>
2023-08-25 16:41:15 +0530

    main

--- EduTechX/src/App.js
+++ EduTechX/src/App.js
@@ -9,6 +9,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import { Contact } from "./pages/Contact";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
@@ -63,6 +64,8 @@ function App() {
             </OpenRoute>
           }
         />  
+        <Route path="/contact"element={<Contact />}/>
+        <Route path="dashboard/my-profile"
     </Routes>
    </div>
   );
