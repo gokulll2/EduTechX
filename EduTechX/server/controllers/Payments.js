cc82bcc294f8c1d00b0169a1ebc1e863983092d3
Gokul <gokulsethi69@gmail.com>
2023-09-20 13:26:47 +0530

    main

--- EduTechX/server/controllers/Payments.js
+++ EduTechX/server/controllers/Payments.js
@@ -7,78 +7,107 @@ const mailSender = require("../utils/mailSender")
 
 //capture the payment and inititate the razorpay order
 exports.capturePayment = async (req,res)=>{
-        //get courseID and userID
-        const {course_id} = req.body;
-        const userID = req.user.id;
-        //validation
-        if(!course_id)
-        {
-            return res.status(400).json({
-                success:false,
-                message:"Please provide valid courseID",
-            })
-        };
-        //valid CourseDetails
-         let course;
-         try{
-            course = await Course.findById(course_id);
+    const {courses} = req.body;
+    const userId = req.user.id;
+    if(courses.length===0)
+    {
+        return res.json({
+            success:false,
+            message:"Please provide Course Id",
+        })
+    }
+    let totalAmount=0;
+
+    for(const course_id of courses)
+    {
+        let course;
+        try{
+            course = await Course.findById(course_id)
             if(!course)
             {
-                return res.status(400).json({
+                return res.status(200).json({
                     success:false,
-                    message:'Could not find the course',
+                    message:"Could not find the course"
                 })
             }
-            //user already pay for the same course
-            const uid = new mongoose.Types.ObjectId(userID);
-         if(course.studentsEnrolled.includes(uid)) 
-         {
-            return res.status(400).json({
-                success:false,
-                message:"Student already registered",
-            })
-         } 
-         } catch(error)
-         {
-            console.log(error);
-            return res.status(400).json({
-                success:false,
-                message:error.message,
-            })
-         }
-         //order create
-         const amount = course.price;
-         const currency = "INR";
-         const options = {
-            amount:amount*100,
-            currency,
-            receiptNo:Math.random(Date.now().toString()),
-            notes:{
-                courseId:course_id,
-                userID,
-            }
-         }
-         try{
-            //initiate the payment using razorpay
-            const paymentResponse = await instance.orders.create(options);
-            console.log(paymentResponse);
+        }
+        catch(err)
+        {
 
-            return res.status(200).json({
-                success:true,
-                courseName:course.courseName,
-                courseDescription:course.courseDescription,
-                thumbnail:course.thumbnail,
-                orderId:paymentResponse.id,
-                currency:paymentResponse.currency,
-                amount:paymentResponse.amount,
-             })
-         } catch(err){
-            console.log(err);
-            return res.status(500).json({
-                success:false,
-                message:"Could not initiate order",
-            });
-         }      
+        }
+    }
+        // //get courseID and userID
+        // const {course_id} = req.body;
+        // const userID = req.user.id;
+        // //validation
+        // if(!course_id)
+        // {
+        //     return res.status(400).json({
+        //         success:false,
+        //         message:"Please provide valid courseID",
+        //     })
+        // };
+        // //valid CourseDetails
+        //  let course;
+        //  try{
+        //     course = await Course.findById(course_id);
+        //     if(!course)
+        //     {
+        //         return res.status(400).json({
+        //             success:false,
+        //             message:'Could not find the course',
+        //         })
+        //     }
+        //     //user already pay for the same course
+        //     const uid = new mongoose.Types.ObjectId(userID);
+        //  if(course.studentsEnrolled.includes(uid)) 
+        //  {
+        //     return res.status(400).json({
+        //         success:false,
+        //         message:"Student already registered",
+        //     })
+        //  } 
+        //  } catch(error)
+        //  {
+        //     console.log(error);
+        //     return res.status(400).json({
+        //         success:false,
+        //         message:error.message,
+        //     })
+        //  }
+        //  //order create
+        //  const amount = course.price;
+        //  const currency = "INR";
+        //  const options = {
+        //     amount:amount*100,
+        //     currency,
+        //     receiptNo:Math.random(Date.now().toString()),
+        //     notes:{
+        //         courseId:course_id,
+        //         userID,
+        //     }
+        //  }
+        //  try{
+        //     //initiate the payment using razorpay
+        //     const paymentResponse = await instance.orders.create(options);
+        //     console.log(paymentResponse);
+
+        //     return res.status(200).json({
+        //         success:true,
+        //         courseName:course.courseName,
+        //         courseDescription:course.courseDescription,
+        //         thumbnail:course.thumbnail,
+        //         orderId:paymentResponse.id,
+        //         currency:paymentResponse.currency,
+        //         amount:paymentResponse.amount,
+        //      })
+        //  } catch(err){
+        //     console.log(err);
+        //     return res.status(500).json({
+        //         success:false,
+        //         message:"Could not initiate order",
+        //     });
+        //  }      
 }
 
 //Verify signature of Razorpay and server
