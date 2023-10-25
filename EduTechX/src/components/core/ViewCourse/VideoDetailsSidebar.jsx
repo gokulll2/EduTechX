f539a0ab33a4a45ff1d5007e8ca35f6b236972ea
Gokul <gokulsethi69@gmail.com>
2023-10-25 23:03:07 +0530

    Final Changes

---
+++ EduTechX/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const VideoDetailsSidebar = () => {
+  return (
+    <div>VideoDetailsSidebar</div>
+  )
+}
--- EduTechX/src/pages/ViewCourse.jsx
+++ EduTechX/src/pages/ViewCourse.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const ViewCourse = () => {
   return (
-    <div>ViewCourse</div>
+    <div>
+        
+    </div>
   )
 }
