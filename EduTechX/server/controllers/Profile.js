eb67b1d72e2ca5ad9e39788e358bc5da33249bf9
Gokul <gokulsethi69@gmail.com>
2023-10-31 11:38:28 +0530

    handling getEnrolledCourses

--- EduTechX/server/controllers/Profile.js
+++ EduTechX/server/controllers/Profile.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const Profile = require("../models/Profile");
 const { uploadImageToCloudinary } = require("../utils/imageUploader");
+const Course = require("../models/Course");
 
 exports.updatedProfile = async(req,res)=>{
     try{
@@ -125,28 +126,96 @@ exports.updateDisplayPicture = async (req,res)=>{
     }
 }
 exports.getEnrolledCourses = async (req,res)=>{
-    try{
-        const userId = req.user.id;
-        const userDetails = User.findOne({
-            _id:userId,
-        }).populate("courses")
-        .exec()
-        if(!userDetails)
-        {
-            return res.status(404).json({
-                success:false,
-                message:`Could not find user with id ${userDetails}`
-            })
+    try {
+        const userId = req.user.id
+        let userDetails = await User.findOne({
+          _id: userId,
+        })
+          .populate({
+            path: "courses",
+            populate: {
+              path: "courseContent",
+              populate: {
+                path: "subSection",
+              },
+            },
+          })
+          .exec()
+  
+        userDetails = userDetails.toObject()
+        var SubsectionLength = 0
+        for (var i = 0; i < userDetails.courses.length; i++) {
+          let totalDurationInSeconds = 0
+          SubsectionLength = 0
+          for (var j = 0; j < userDetails.courses[i].courseContent.length; j++) {
+            totalDurationInSeconds += userDetails.courses[i].courseContent[
+              j
+            ].subSection.reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
+            userDetails.courses[i].totalDuration = convertSecondsToDuration(
+              totalDurationInSeconds
+            )
+            SubsectionLength +=
+              userDetails.courses[i].courseContent[j].subSection.length
+          }
+          let courseProgressCount = await CourseProgress.findOne({
+            courseID: userDetails.courses[i]._id,
+            userId: userId,
+          })
+          courseProgressCount = courseProgressCount?.completedVideos.length
+          if (SubsectionLength === 0) {
+            userDetails.courses[i].progressPercentage = 100
+          } else {
+            // To make it up to 2 decimal point
+            const multiplier = Math.pow(10, 2)
+            userDetails.courses[i].progressPercentage =
+              Math.round(
+                (courseProgressCount / SubsectionLength) * 100 * multiplier
+              ) / multiplier
+          }
+        }
+    
+        if (!userDetails) {
+          return res.status(400).json({
+            success: false,
+            message: `Could not find user with id: ${userDetails}`,
+          })
         }
         return res.status(200).json({
-            success:true,
-            data:userDetails.courses,
+          success: true,
+          data: userDetails.courses,
         })
-    } catch(err)
-    {
+      } catch (error) {
         return res.status(500).json({
+          success: false,
+          message: error.message,
+        })
+      }
+}
+exports.instructorDashboard = async (req,res)=>{
+    try{
+        const courseDetails = await Course.find({instructor:req.user.id});
+        const courseData = courseDetails.map((course)=>{
+            const totalStudentsenrolled = course.studentsEnrolled.length
+            const totalAmountGenerated = totalStudentsenrolled*course.price
+
+            const courseDataWithStats = {
+                _id:course._id,
+                courseName:course.courseName,
+                courseDescription:course.courseDescription,
+                totalStudentsenrolled,
+                totalAmountGenerated,
+            }
+            return courseDataWithStats
+        })
+        return res.status(200).json({
+            success:true,
+            courses:courseData,
+        })
+    }catch(err){
+        console.log(err);
+        return res.status(404).json({
             success:false,
-            message:err.message,
+            message:"Internal Server Error",
         })
     }
 }
